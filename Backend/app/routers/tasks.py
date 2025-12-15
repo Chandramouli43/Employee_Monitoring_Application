fd49e7e995c240618fe144c671f0614bd7f0a1ac
sameer <sameershaikzayn@gmail.com>
2025-12-15 16:35:43 +0530

    task

--- Backend/app/routers/tasks.py
+++ Backend/app/routers/tasks.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException, Depends
 from typing import List
 from sqlalchemy.orm import Session
-import app.models
+from app.models.task import Task
 import app.core.database as database
 from app.schemas.projects import Task, TaskCreate
 
@@ -13,12 +13,12 @@ get_db = database.get_db
 
 @router.get("/", response_model=List[Task])
 def get_tasks(db: Session = Depends(get_db)):
-    return db.query(app.models.Task).all()
+    return db.query(Task).all()
 
 
 @router.post("/", response_model=Task)
 def create_task(request: TaskCreate, db: Session = Depends(get_db)):
-    new_task = app.models.Task(**request.dict())
+    new_task = Task(**request.dict())
     db.add(new_task)
     db.commit()
     db.refresh(new_task)
@@ -27,7 +27,7 @@ def create_task(request: TaskCreate, db: Session = Depends(get_db)):
 
 @router.put("/{task_id}", response_model=Task)
 def update_task(task_id: int, request: TaskCreate, db: Session = Depends(get_db)):
-    task = db.query(app.models.Task).filter(app.models.Task.id == task_id).first()
+    task = db.query(Task).filter(Task.id == task_id).first()
     if not task:
         raise HTTPException(status_code=404, detail="Task not found")
     for key, value in request.dict().items():
@@ -38,7 +38,7 @@ def update_task(task_id: int, request: TaskCreate, db: Session = Depends(get_db)
 
 @router.delete("/{task_id}")
 def delete_task(task_id: int, db: Session = Depends(get_db)):
-    task = db.query(app.models.Task).filter(app.models.Task.id == task_id).first()
+    task = db.query(Task).filter(Task.id == task_id).first()
     if not task:
         raise HTTPException(status_code=404, detail="Task not found")
     db.delete(task)
